315e28de15beede0063f56f3c7caabef71826f71
Marco Pegoraro <marco.pegoraro@gmail.com>
2016-08-24 16:37:29 +0200

    Add API server

---
+++ projects/wordpress/apps/api_server/app.js
@@ -0,0 +1,25 @@
+
+// is it worth to add an in-memory caching layer?
+// something that lasts few minutes?
+
+var express = require('express');
+var redis = require('./lib/redis');
+
+var expressApp = express();
+var port = process.env.PORT || 8080;
+
+expressApp.get('/', (req, res) => {
+    redis.get('posts')
+    .then(msg => res.send(msg))
+    .catch(err => res.status(500).send(err));
+});
+
+expressApp.get('/:id', (req, res) => {
+    redis.get('post-' + req.params.id)
+    .then(msg => res.send(msg))
+    .catch(err => res.status(500).send(err));
+});
+
+expressApp.listen(port, function () {
+    console.log(new Date(), 'Frontend API app listening on port ' + port + '!');
+});
